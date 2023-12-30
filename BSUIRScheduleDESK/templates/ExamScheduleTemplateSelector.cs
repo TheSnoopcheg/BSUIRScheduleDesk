23ae34c88c444a08c6526109da895ae01ebe4f70
TheSnoopcheg <dimapartizan5@gmail.com>
2023-12-30 16:11:41 +0300

    exam announcement added

---
+++ BSUIRScheduleDESK/templates/ExamScheduleTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using BSUIRScheduleDESK.classes;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BSUIRScheduleDESK.templates
+{
+    public class ExamScheduleTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement? element = container as FrameworkElement;
+            if (element != null && item != null && item is Schedule)
+            {
+                Schedule? schedule = item as Schedule;
+                if (schedule != null)
+                {
+                    if (schedule.announcement)
+                    {
+                        return element.FindResource("announcementTemplate") as DataTemplate;
+                    }
+                    else
+                    {
+                        return element.FindResource("scheduleTemplate") as DataTemplate;
+                    }
+                }
+            }
+            return element.FindResource("emptyTemplate") as DataTemplate;
+        }
+
+    }
+}
