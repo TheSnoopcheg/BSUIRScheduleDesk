a2ed72b0916d1d718bfcc58cef914f89182765b4
TheSnoopcheg <dimapartizan5@gmail.com>
2026-02-21 23:27:32 +0300

    better employee info presentation

---
+++ BSUIRScheduleDESK/Converters/HtmlTextConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BSUIRScheduleDESK.Converters;
+
+public class HtmlTextConverter : IValueConverter
+{
+    public static readonly HtmlTextConverter Instance = new HtmlTextConverter();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string text) return value;
+        text.Insert(0, "<div>");
+        text += "</div>";
+        return text;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
